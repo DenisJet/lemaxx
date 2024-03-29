bc45a5390328a39e8b3344ec03e48eea290c9804
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-29 11:59:24 +0500

    add modal close button

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -14,7 +14,17 @@ export default function Modal({ active, setActive }: ModalProps): JSX.Element {
         className={active ? `${styles.modalContent} ${styles.active}` : styles.modalContent}
         onClick={(evt) => evt.stopPropagation()}
       >
-        <p>Оставьте заявку и получите 10% скидку</p>
+        <button type='button' title='Закрыть' className={styles.button} onClick={() => setActive(false)}>
+          <svg width='16' height='16' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg'>
+            <path
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+              d='M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z'
+              fill='#FFFFFF'
+            ></path>
+          </svg>
+        </button>
+        <p>Отправте свой номер телефона и мы перезвоним Вам в ближайшее время</p>
         <Form />
       </div>
     </div>
